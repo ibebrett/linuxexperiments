d7dfb0f00bf7c7515f7fe1ce164720d5754a6210
ibebrett <ibebrett@gmail.com>
2016-08-14 14:07:41 -0400

    Reformatting some code, as well as adding a readme

--- dynamic_linking/dl.c
+++ dynamic_linking/dl.c
@@ -2,14 +2,14 @@
 #include <dlfcn.h>
 
 int main() {
-    void* dl_handle;
-    typedef float (*fptr)(float);
-    fptr func;
-    char* error;
+  void* dl_handle;
+  typedef float (*fptr)(float);
+  fptr func;
+  char* error;
 
-    dl_handle = dlopen("libm.so.6", RTLD_LAZY);
-    func = reinterpret_cast<fptr>(dlsym(dl_handle, "cosf"));
-    std::cout << "output of func" << (*func)(0.0f);
-    std::cout << "output of func" << (*func)(0.5f);
-    dlclose(dl_handle);
+  dl_handle = dlopen("libm.so.6", RTLD_LAZY);
+  func = reinterpret_cast<fptr>(dlsym(dl_handle, "cosf"));
+  std::cout << "output of func" << (*func)(0.0f);
+  std::cout << "output of func" << (*func)(0.5f);
+  dlclose(dl_handle);
 }
